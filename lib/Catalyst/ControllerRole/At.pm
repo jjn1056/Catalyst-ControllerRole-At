6513af1370673e79fe2c0fa34c3da613f868322c
John Napiorkowski <jjn1056@yahoo.com>
2016-02-04 10:11:21 -0600

    more docs

--- lib/Catalyst/ControllerRole/At.pm
+++ lib/Catalyst/ControllerRole/At.pm
@@ -10,43 +10,57 @@ Catalyst::ControllerRole::At - A new approach to building Catalyst actions
 
 =head1 SYNOPSIS
 
-    package MyApp::Controller::Example;
+    package MyApp::Controller::User;
 
     use Moose;
     use MooseX::MethodAttributes;
+    use Types::Standard qw/Int Str/;
 
     extends 'Catalyst::Controller';
     with 'Catalyst::ControllerRole::At';
 
-    At($controller/foo) => Chained(/) PathPart($controller/foo) Args(0) { ... }
-    At($controller/foo/{*}) => Chained(/) PathPart($controller/foo) Args { ... }
-    At($controller/foo/{}) => Chained(/) PathPart($controller/foo) Args(1) { ... }
-    At($controller/foo/{:Int}) => Chained(/) PathPart($controller/foo) Args(Int) { ... }
-    At($controller/foo/{id:Int}) => 
-    Chained(/) PathPart($controller/foo) Args(Int) Does(NamedFields) Field(id=>$args[0]) { ... }
+    # Define your actions, for example:
 
+    sub list :At($action?{q:Str}) { ... }
+
+    sub find :At($controller/{id:Int}) { ... }
 
     __PACKAGE__->meta->make_immutable;
+
+Makes two routes matching:
+
+    https://localhost/user/list?q=byage
+    https://localhost/user/100
 
 =head1 DESCRIPTION
 
 The way L<Catalyst> uses method attributes to annote a subroutine with meta
-information used to map that action to a URL template has often been difficult
-for newcomers to the framework, compared to more straightforward systems seen in
-other popular frameworks.  Partly this is due to how the system evolved and was
-augmented, with more care towards backcompatibility (for example with L<Maypole>, its
+information used to map that action to an incoming request has sometimes been difficult
+for newcomers to the framework.  Partly this is due to how the system evolved and was
+augmented, with more care towards backwards compatibility (for example with L<Maypole>, its
 architectural anscestor) than with designing a forward system that is easy to grasp.
 Additionally aspects of the system such as chained dispatch are very useful in the
-hands of an expert but the interface leaves a lot to be desired.
+hands of an expert but the interface leaves a lot to be desired.  For example it is
+possible to craft actions that mix chaining syntax with 'classic' syntax in ways that
+are confusing.  And having more than one way to do the same thing without clear and
+obvious benefits is confusing to newcomers.
 
-Lastly, the core L<Catalyst> syntax has confusing defaults that are not readily guessed.
+Lastly, the core L<Catalyst::Controller> syntax has confusing defaults that are not readily guessed.
 For example do you know the difference (if any) between Args and Args()?  Or the difference
 between Path, Path(''), and Path()?  In many cases defaults are applied that were not
-intended.  This alternative syntax for the most part eliminates defaults and guessed
-intentions.
+intended and things that you might think are the same turn out to have different effects.  All
+this conspires to worsen the learning curve.
 
-This ControllerRole is an attempt to layer some sugar on top of the existing interface
-with the hope to establishing a normalized, easy approach that doesn't have the
+This role defines an alternative syntax that we hope is easier to understand and for the most
+part eliminates defaults and guessed intentions.  It only defines two method attributes, "At()"
+and "Via()", which have no defaults and one of which is always required.  It also smooths
+over differences between 'classic' route matching using :Local and :Path and the newer
+syntax based on Chaining by providing a single approach that bridges between the two
+styles.  One can mix and match the two without being required to learn a new syntax or to
+rearchitect the system.
+
+Ultimately this ControllerRole is an attempt to layer some sugar on top of the existing
+interface with the hope to establishing a normalized, easy approach that doesn't have the
 learning curve or confusion of the existing system.
 
 I also recommend reading L<Catalyst::RouteMatching> for general notes and details on
@@ -60,6 +74,8 @@ the root of your website domain (https://localhost/, not https://localhost/somep
 
 =head2 Matching a Literal Path
 
+The action 'global_path' will respond to 'https://localhost/foo/bar/baz'.
+
     package MyApp::Controller::Example;
 
     use Moose;
@@ -72,7 +88,10 @@ the root of your website domain (https://localhost/, not https://localhost/somep
 
     __PACKAGE__->meta->make_immutable;
 
-The action 'global_path' will respond to 'https://localhost/foo/bar/baz'.
+The main two parts are consuming the role c< with 'Catalyst::ControllerRole::At'>
+and using the C<At> method attribute.  This attribute can only appear once in your
+action and should be string that matches a specification as to be described in the
+following examples.
 
 =head2 Arguments in a Path specification
 
@@ -113,6 +132,10 @@ the above two actions) should the request be 'https://localhost/example/foo/bar'
 the first action 'arg2' would match since its a better match for that request given it
 has a more constrained specification. In general I recommend using '{@}' sparingly.
 
+B<NOTE> Placeholder must come after path part literals or expansion variables as discussed
+below.  For example "At(/bar/{}/bar)" is not valid.  This type of match is possible with
+chained actions (see more examples below).
+
 =head2 Naming your Arguments
 
 You may name your argument placeholders.  If you do so you can access your argument
@@ -124,7 +147,10 @@ placeholder values via the %_ hash.  For example:
     }
 
 Note that regardless of whether you name your arguments or not, they will get passed to
-your actions at request via @_, as in core L<Catalyst>.
+your actions at request via @_, as in core L<Catalyst>.  So in the above example '$id'
+is equal to '$_{id}'.  You may use whichever makes the most sense for your task, or
+standardize a project on one form or the other.  You might also find naming the arguments
+to be a useful form of documentation.
 
 =head2 Type constraints on your Arguments
 
@@ -148,7 +174,7 @@ You may leverage the built in support for applying type constraints on your argu
 Would match 'http://localhost/example/100' but not 'http://localhost/example/string'
 
 All the same rules that apply to L<Catalyst> regarding use of type constraints apply.  Most
-importantly you must remember in inport your type constraints, as in the above example.  You
+importantly you must remember to inport your type constraints, as in the above example.  You
 should consider reviewing L<Catalyst::RouteMatching> for more general help.
 
 You may declare a type constraint on an argument but not name it, as in the following
@@ -170,9 +196,23 @@ to make this and other common action template patterns easier, we support the fo
 variable expansions in your URL template specification:
 
     $controller: Your controller namespace (as an absolute path)
-    $action: the subroutine name of your action.
-    $here: expands the same way as "$controller/$action"
-    $local: expands the same way as "$controller/$action/{*}"
+    $up: The namespace of the controller containing this controller
+    $subname The name of your action (the subroutine name)
+    $action: The action namespace (same as $controller/$method)
+
+For example if your controller is 'MyApp::Controller::User::Details' then:
+
+    $controller => /user/details
+    $up => /user
+
+And if 'MyApp::Controller::User::Details' contained an action like:
+
+    sub list :At() { ... }
+
+then:
+
+    $subname => list
+    $action => /user/details/list
 
 You use these variable expansions the same way as literal paths:
 
@@ -189,12 +229,12 @@ You use these variable expansions the same way as literal paths:
       my ($self, $c, $id) = @_;     
     }
 
-    sub list :At($local) { ... }
+    sub list :At($subname) { ... }
 
     __PACKAGE__->meta->make_immutable;
 
 In this example the action 'args' would match 'https://localhost/example/100' (with '100' being
-considered an argument) while action 'list' would match 'https::/localhost/example/list/..'.
+considered an argument) while action 'list' would match 'https::/localhost/example/list'.
 
 You can use expansion variables in your base controllers or controller roles to more
 easily make shared actions.
@@ -206,6 +246,12 @@ behavior is to produce a namespace like the following:
 
     package MyApp::Controller::User => /user
     package MyApp::Controller::User::name => /user/name
+
+Changing the way a controller defines its namespace will also change how actions that are
+defined in that controller defines thier namespaces.
+
+B<NOTE> WHen using expansions, you should not place a '/' at the start of your
+template URI.
 
 =head2 Matching GET parameters
 
@@ -220,7 +266,7 @@ You can match GET (query) parameters in your URL template definitions:
     extends 'Catalyst::Controller';
     with 'Catalyst::ControllerRole::At';
 
-    sub query :At($here?{name:Str}{age:Int}) {
+    sub query :At($action?{name:Str}{age:Int}) {
       my ($self, $c, $id) = @_;     
     }
 
@@ -236,16 +282,43 @@ all that is required is that the requested query parameters exist.
 This uses the ActionRole L<Catalyst::ActionRole::QueryParameter> under the hood, which
 you may wish to review for more details.
 
+=head2 Matching GET parameters against an interface
+
+In some cases it might be preferred to match GET parameters to an interface, rather than
+to name and apply constraints to a long list of parameters.  This can promote better code
+reuse and conciseness at some cost to immediate understanding.  For example:
+
+    package MyApp::Controller::Example;
+
+    use Moose;
+    use MooseX::MethodAttributes;
+    use MyApp::Types qw/SearchParams/;
+
+    extends 'Catalyst::Controller';
+    with 'Catalyst::ControllerRole::At';
+
+    sub query :At($action?{@:SearchParams}) {
+      my ($self, $c, $id) = @_;     
+    }
+
+    __PACKAGE__->meta->make_immutable;
+
+In this case the '@' indicates that the constraint C<SearchParams> is to recieve all the GET parameters
+as a reference (it basically gets the value stored in $c->request->query_parameters).  
+
+Please see L<Catalyst::ActionRole::QueryParameter>  for more.
+
 =head2 Chaining Actions inside a Controller
 
 L<Catalyst> action chaining allows you to spread the logic associated with a given URL
 across a set of actions which all are responsible for handling a part of the URL
 template.  The idea is to allow you to better decompose your logic to promote clarity
-and reuse.  However the built in syntax for declaring action chains leaves a lot to be
-desired.  Here's how you do it with L<Catalyst::ControllerRole::At>
+and reuse.  However the built-in syntax for declaring action chains is difficult for
+many people to use.  Here's how you do it with L<Catalyst::ControllerRole::At>
 
 Starting a Chain of actions is straightforward.  you just add '/...' to the end of your
-path specification.  For example:
+path specification.  This is to indicate that the action expects more parts 'to follow'.
+For example:
 
     package MyApp::Controller::Example;
 
@@ -261,7 +334,7 @@ path specification.  For example:
     __PACKAGE__->meta->make_immutable;
 
 The action 'init' starts a new chain of actions and declares the first part of the
-definition, 'https://localhost/example/...'.  You continue an chain in the same way,
+definition, 'https://localhost/example/...'.  You continue a chain in the same way,
 but you need to specify the parent action that is being continued using the 'Via'
 attribute.  You terminate a chain when you define an action that doesn't declare '...'
 as the last path.  For example:
@@ -279,8 +352,11 @@ as the last path.  For example:
         }
 
 This defines an action chain with three 'stops' which matches a URL like (for example)
-'https://localhost/$controller/arg1/arg2'.
+'https://localhost/$controller/arg1/arg2'.  Each action will get executed for the matching
+part, and will get arguments as defined in their match specification.
 
+B<NOTE> The 'Via' attribute must contain a value.
+ 
 When chaining you can use (or not) any mix of type constraints on your arguments, named
 arguments, and query parameter matching.  Here's a full example:
 
@@ -296,11 +372,11 @@ arguments, and query parameter matching.  Here's a full example:
     sub init :At($controller/...) { ... }
 
       sub next :Via(init) At({id:Int}/...) {
-        my ($self, $c, $id) = @_;
+        my ($self, $c, $int_id) = @_;
       }
 
-        sub last :Via(next) At({id:Int}) {
-          my ($self, $c, $id) = @_;
+        sub last :Via(next) At({id:Int}?{q}) {
+          my ($self, $c, $int_id) = @_;
         }
 
     __PACKAGE__->meta->make_immutable;
@@ -334,17 +410,65 @@ case you can omit the path and argument match specification.  For example:
 
 This will match a URL like 'https://localhost/example/100'.
 
-B<NOTE> If you declare a Via but not At, this means the action is a terminal one
-that has no literal path or arguments
+B<NOTE> If you declare a Via but not At, this is an error.  You must
+always provide an At(), even in the case of a terminal action with no
+match parts of it own.  For example:
+
+    package MyApp::Controller::Example;
+
+    use Moose;
+    use MooseX::MethodAttributes;
+
+    extends 'Catalyst::Controller';
+    with 'Catalyst::ControllerRole::At';
+
+    sub first :At($controller/...) { ... }
+
+      sub second :Via(first) At(...) {
+        my ($self, $c) = @_;
+      }
+
+        sub third :Via(second) At(...) {
+          my ($self, $c) = @_;
+        }
+
+          sub last :Via(third) At() {
+            my ($self, $c, $id) = @_;
+          }
+
+    __PACKAGE__->meta->make_immutable;
+
+This creates a chained action that matches 'http://localhost/example' but calls
+each of the three actions in the chain in order.  Although it might seem odd to
+create an action that is not connected to a path part of a URL request, you might find
+cases where this results in well factored and reusable controllers.
+
+B<NOTE> For the purposes of executing code, we treat 'At' and 'At()' as the same.  However
+We highly recommend At() as a best practice since it more clearly represents the idea
+of 'no match template'.
 
 =head2 Chaining Actions across Controllers
 
-For the case when you are continuing your chained actions across controllers
-we provide some template expansions you can use in the 'Via' attribute.
+The method attributes 'Via()' contains a pointer to the action being continued.  In
+standard practice this is almost always the name of an action in the same controller
+as the one declaring it.  This could be said to be a 'relative' (as in relative to
+the current controller) action.  However you don't have to use a relative name.  You
+can use any action's absolute private name, as long as it is an action that declares itself
+to be a link in a chain.
+
+However in practice it is not alway a good idea to spread your chained acions across
+across controllers in a manner that is not easy to follow.  We recommend you try
+to limit youself to chains that follow the controller hierarchy, which should be
+easier for your code maintainers.
+
+For this common, best practice case when you are continuing your chained actions across
+controllers, following a controller hierarchy, we provide some template expansions you can
+use in the 'Via' attribute.  These are useful to enforce this best practice as well as
+promote reusability by decoupling hard coded private action namespaces from your controller.
 
     $up: The controller whose namespace contains the current controller
-    $action: The name of the current actions subroutine
-    $parent: Expands to $up/$action
+    $subname The name of the current actions subroutine
+    $parent: Expands to $up/$subname
 
 For example:
 
@@ -360,7 +484,7 @@ For example:
       my ($self, $c) = @_;
     }
 
-      sub list :Via(init) At($action) {
+      sub list :Via(init) At($subname) {
         my ($self, $c) = @_;
       }
 
@@ -378,9 +502,9 @@ For example:
       my ($self, $c) = @_;
     }
 
-      sub show    :Via(init) At($action) { ... }
-      sub update  :Via(init) At($action) { ... }
-      sub delete  :Via(init) At($action) { ... }
+      sub show    :Via(init) At($subname) { ... }
+      sub update  :Via(init) At($subname) { ... }
+      sub delete  :Via(init) At($subname) { ... }
 
     __PACKAGE__->meta->make_immutable;
 
@@ -390,6 +514,44 @@ This creates four (4) URL templates:
     https://localhost/thingstodo/:id/show
     https://localhost/thingstodo/:id/update
     https://localhost/thingstodo/:id/delete
+
+With an action execution flow as follows:
+
+    https://localhost/thingstodo/list =>
+      /thingstodo/init
+      /thingstodo/list
+
+    https://localhost/thingstodo/:id/show
+      /thingstodo/init
+        /thingstodo/item/init
+        /thingstodo/item/show
+
+    https://localhost/thingstodo/:id/update
+      /thingstodo/init
+        /thingstodo/item/init
+        /thingstodo/item/update
+
+    https://localhost/thingstodo/:id/delete
+      /thingstodo/init
+        /thingstodo/item/init
+        /thingstodo/item/delete
+
+=head1 COOKBOOK
+
+One thing I like to do is create a base controller for my project
+so that I can make my controllers more concise:
+
+    package Myapp::Controller;
+
+    use Moose;
+    extends 'Catalyst::Controller';
+    with 'Catalyst::ControllerRole::At';
+     
+    __PACKAGE__->meta->make_immutable;
+
+You can of course doa  lot more here if you want but I usually recommend
+the lightest touch possible in your base controllers since the more you customize
+the harder it might be for people new to the code to debug the system.
 
 =head1 AUTHOR
  
---
+++ t/at.t
@@ -0,0 +1,336 @@
+BEGIN {
+  use Test::Most;
+  eval "use Catalyst 5.90093; 1" || do {
+    plan skip_all => "Need a newer version of Catalyst => $@";
+  };
+}
+
+{
+  package MyApp::Controller::Root;
+  $INC{'MyApp/Controller/Root.pm'} = __FILE__;
+
+  use base 'Catalyst::Controller';
+
+  sub one :Chained(/) Args(1) { }
+  sub two :Chained(/) PathPart() Args(1) { }
+  sub thr :Chained(/) PathPart('') Args(1) { }
+  
+  MyApp::Controller::Root->config(namespace=>'');
+
+  package MyApp::Controller::Example;
+  $INC{'MyApp/Controller/Example.pm'} = __FILE__;
+
+  use base 'Catalyst::Controller';
+
+  sub one1 :Chained(/) At(controller/{$}/...) Args(1) { }
+  sub two1 :Chained(/) At($controller/{$}/...) PathPart Args(1) { }
+  sub thr1 :Chained(/) At($controller/{$}/...) PathPart('') Args(1) { }
+
+  sub _parse_At_attr {
+    my ( $self, $app, $name, $value ) = @_;
+    my %extra;
+    my @attributes = @{$self->meta->get_method($name)->attributes||[]};
+
+    my $via_expansions = (
+      '..' => ,
+      'parent' => , # ../$action
+    );
+
+    my $args_type = 'Args';
+    my $path_parts;
+
+    my @args = ();
+    my 
+    foreach my $attr(@attributes) {
+      if(my ($val) = $attr=~m/^At\((.+)\)$/i) {
+        my ($path, $query) = split('\?', $val);
+        my @parts = split '/', $val;
+        
+        foreach my $part (@parts) {
+          $path_parts .= '/' if $part eq '';
+          $path_parts .= $self->path_prefix.'/' if $part eq '$controller';
+          $path_parts .= $self->path_prefix.'/' if $part eq '.';
+          $part_parts .= $name.'/' if $path eq '$action';
+          $part_parts .= $self->path_prefix .'/'. $name .'/' if $path eq '$location';
+          $args_type = 'CaptureArgs' if $path = '...';
+        }
+
+        
+      } elsif(my ($val) = $attr=~/^Via\((.+)\)$/i) {
+        warn $val;
+      }
+    }
+
+use Devel::Dwarn;
+#Dwarn \@attributes;
+#Dwarn \@_;
+
+Dwarn \%extra;
+Dwarn $args_type;
+Dwarn $path_parts;
+
+    return ();# %extra;
+  }
+
+  #MyApp::Controller::Example->config(namespace=>'ee');
+
+  package MyApp;
+  use Catalyst;
+  
+  MyApp->setup;
+}
+
+use HTTP::Request::Common;
+use Catalyst::Test 'MyApp';
+
+warn MyApp->controller('Root')->path_prefix;
+warn MyApp->controller('Root')->action_namespace;
+warn MyApp->controller('Example')->path_prefix;
+warn MyApp->controller('Example')->action_namespace;
+
+ok 1;
+
+#{
+  #ok my $res = request GET "/root/echo?q=foo bar baz ♥";
+  #is_deeply eval $res->content, [qw/foo bar baz ♥/];
+#}
+   
+done_testing;
+
+__END__
+
+PathPart and PathPart() are the same
+No given PathPart is the same as saying PathPart (or PathPart()).
+All three mean 'pathpart is the action name'.   Keep in mind its
+just the action name, not the controller namespace + name (not like Local)
+
+But PathPart('') means 'no part part'.
+h
+*PathPrefix is path of Controller Prefix, which is derived from namespace...*
+PathPrefix ignores the action name totally
+
+Chained(/) starts a chain.  All Chains start from the root of the domain.  
+
+Chained(/ddd) is under /ddd (absolute path)
+Chained(ddd) is under something called ddd in the current controller
+Chained(../ddd) is 'one level up' to ddd
+ChainedParent is a special like Chained(.../$actionname)
+
+!!!!!1
+At($controller/foo) => Chained(/) PathPart($controller/foo) Args(0) { ... }
+At($controller/foo/{*}) => Chained(/) PathPart($controller/foo) Args { ... }
+At($controller/foo/{}) => Chained(/) PathPart($controller/foo) Args(1) { ... }
+At($controller/foo/{:Int}) => Chained(/) PathPart($controller/foo) Args(Int) { ... }
+At($controller/foo/{id:Int}) => 
+  Chained(/) PathPart($controller/foo) Args(Int) Does(NamedFields) Field(id=>$args[0]) { ... }
+
+
+At($controller/foo/...) => Chained(/) PathPart($controller/foo) CaptureArgs(0) { ... }
+At($controller/foo/{}/...) => Chained(/) PathPart($controller/foo) CaptureArgs(1) { ... }
+At($controller/foo/{:Int}/...) => Chained(/) PathPart($controller/foo) CaptureArgs(Int) { ... }
+At($controller/foo/{id:Int}/...) => Chained(/) PathPart($controller/foo) CaptureArgs(Int) { ... }
+
+
+At(./{id:Int}?{page:Int}%{name:Str}{age:Int}) 
+At(./{id:Int}%{:UserForm}*) ????
+
+  Via(..) => Chained(..) PathPart('') Args(0) { ... } ?????
+
+  # /root/*
+  sub root :Chained(/) PathPart('root') CaptureArgs(1) {
+
+  }
+     /root/*  +      =>  /root/*/*
+    sub myaction :Chained(root) PathParts('') Args(0) {
+    }
+
+
+  sub root :At(root/...) { ... }
+  sub root :At($controller/root/...) { ... }
+  sub root :At($controller/root/$next) { ... } 
+
+    sub under :Via(root) At({}) { ... }
+
+
+At(:namespace/...)
+At(:ns/:actionname/...
+At(:local/...) sub myaction :Chained(/) PathPart($controller/myaction) CaptureArgs(0) { ... }
+At(:local/{id:Int}/...) sub myaction :Chained(/) PathPart($controller/myaction) CaptureArgs(0) { ... }
+
+
+At(:local/{$*})  sub myaction :Local Args { ... }
+
+At(:controller/:action/{id:Int})
+At(./:action/{id:Int})
+
+At(./:action/{}/)
+
+Under(root) At(a/b/c/{id}/...)
+Under(root) At($action/{id}/{date}
+/)
+
+
+sub myaction($view, $model) :At($local/{id}) {
+  $view->ok($model->find($id));
+}
+
+  NamedFields(id=>$args[0])
+  
+sub myaction :At($local/{id:Int}?{page}{order:Int}) {
+  $_->view->ok($_->model->find($_{id})
+    || $_->detach('/not_found'));
+}
+
+  sub notfound :At($local/{id:Int}?{page}&{order:Int}) {
+    $_->view->not_found(message => "Id $_{id} is not in the database");
+  }
+
+  sub myaction :At($local/{u:User}?{page}&{order:Int}) {
+    $_->view->ok($_{u});
+  }
+
+sub myaction($view, $model M::Schema::User::Result) :At($local/{id}) {
+  $view->ok($model->find($id));
+}
+
+
+Via(action)
+Via(/path/to)
+Via(../to)
+Via(../:action)
+Via(:up/:action)
+
+
+At(:action/{id:Int}?{sort='desc':SortEnum})
+
+
+Take what is inside Via, expand placeholders, and set Chained($val);
+Take what is inside At
+  - if Via doesn't exist, do Chained(/)
+  - figure out any pathparts (after expanding placeholders
+  - figure out any Args(X), Args or CaptureArgs(x) (including Constraints).
+  - Add actionroles for  Named Args as need,
+
+An At has a path part section an args or capture args section (look for trailing ...)
+  and a query section.
+
+package MyApp::Controller::Example;
+
+use Moose;
+use MooseX::MethodAttributes;
+
+extends 'Catalyst::Controller';
+
+sub root :At($controller/...) { }
+
+  sub endpoint :Via(root) At({id}) {
+    my ($self, $c, $id) = @_;
+  }
+
+  sub endpoin2 :Under(root) At({id}/{@}) {
+    my ($self, $c, @args) = @_;
+  }
+
+__PACKAGE__->meta->make_immutable;
+
+package MyApp::Controller::Example;
+
+use Moose;
+use MooseX::MethodAttributes;
+
+extends 'Catalyst::Controller';
+
+sub root Chained(/) PathPrefix CaptureArgs(0) { }
+
+  sub endpoint :Chained(root) PathPrefix('') Args(1) {
+    my ($self, $c, $id) = @_;
+  }
+
+  sub endpoin2 :Chained(root) PathPrefix('') Args {
+    my ($self, $c, @args) = @_;
+  }
+
+__PACKAGE__->meta->make_immutable;
+
+
+__END__
+
+?? What URL paths are mapped here
+?? what does 'warn $_{id}' do?
+?? Would :Via work as well as :Under?
+
+
+At(/foo/{$:User.0}/{$:User.1}%{$:User.age}{$:User.name}
+At(/user/{id:User}%{params*:User}
+
+If a Type::Foo accepts context, then $c is part of any coercions
+
+sub myaction(User $u) :At(/user/{id:User}) {
+
+}
+
+
+sub foo :Local Named(foo=>$arg[0],bar=>$query{id}) 
+
+
+sub foo :At(./{id:User}) { ... }
+sub foo :At(./{id:User,User.email}) { ... }
+sub foo :At(./{id:User,User.email}) { ... }
+
+
+sub user :At(./{id:User} { }
+  sub friend :Via(user) At({id:Friend[user:User.id
+
+
+#######
+
+sub myaction([$id1,$id2]->User $u) At(users/{id1:Int}/{id2:Int})
+sub myaction([Arg,Arg]->User $u) At(users/{id1:Int}/{id2:Int})
+
+# User isa Model::Type::User
+
+sub myaction(User $u) Via(root) At(users/{User.0:id1}/{User.1:id2})
+{
+  $_{User} isa Model::Schema::User::Result
+}
+
+OR
+
+sub myaction([id1,id2]->User $u ==> UserResultSet) 
+    Via(root) At(users/{id1:Int}/{id2:Int})
+{
+  $_{User} isa Model::Schema::User::Result
+}
+
+is User->coerce([id1,id2])
+
+Model::Types::User
+
+sub ACCEPT_CONTENT {
+
+  my $UserRS = $c->model->isa('UserRS') 
+    ? $c->model : $c->model(Schema::User)
+
+  User isa Ref Model::User::Result
+  coerce from [Int,Int]
+  via $c->model(User)->find($_[0],$_[1])
+
+
+######
+
+__PACKAGE__->register_actions(
+  Path {
+    'foo',
+    Action {
+      my ($self, $c) = @;
+    },
+  }
+
+
+);
+
+action user(Int->User $u => User) At($local/{id:Int}) {
+  return $u;
+}
+
+  return $ctx->created($u)
